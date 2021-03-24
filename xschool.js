c42a879ac4eaad817ad353311f7fb9ba78fbff63
FarrasDK <farrooos013@gmail.com>
2021-03-24 17:20:33 +0700

    upload 1

---
+++ xschool.js
@@ -0,0 +1,13 @@
+var playButton1 = document.getElementById("playbutton1")
+var playButton2 = document.getElementById("playbutton2")
+
+
+
+document.getElementById("playbutton1").addEventListener('click', function(){
+    playButton1.style.display = 'none'
+})
+
+document.getElementById("playbutton2").addEventListener('click', function(){
+    playButton2.style.display = 'none'
+})
+
